7b40574c3b3290c99906e71e0b7513dd55a3f6ed
Israilov Sardor <Sardor_09062002@mail.ru>
2022-07-17 19:34:05 +0600

    finished first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ import "../styles/style.scss";
 import { getLogin } from "./login";
 import { getUserLocal } from "./usersName";
 import { transitionPage } from "./transitionPage";
+import { modeLogin } from "./modeSign";
+modeLogin()
 transitionPage()
 getUserLocal()
 getLogin()
